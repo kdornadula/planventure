52a2317fbf6a7d7fdf735b26708d480552a6cf01
Krishnamoorthy Dornadula <dkmoorthy@gmail.com>
2025-10-05 22:39:22 +0530

    Add WSGI configuration for PythonAnywhere deployment
    - Create wsgi.py for PythonAnywhere web app
    - Configure Python path for production environment
    - Enable Flask app deployment on PythonAnywhere platform

--- planventure-api/wsgi.py
+++ planventure-api/wsgi.py
@@ -2,9 +2,9 @@ import sys
 import os
 
 # Add your project directory to sys.path
-path = '/home/kdornadula/planventure/planventure-api'
-if path not in sys.path:
-    sys.path.append(path)
+project_home = '/home/kdornadula/planventure/planventure-api'
+if project_home not in sys.path:
+    sys.path = [project_home] + sys.path
 
 from app import app as application
 
